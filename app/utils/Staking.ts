78c133e9ec79298681f2a954861090da437b8ade
martell <anantabhsahu77.com>
2024-11-09 01:23:42 +0530

    protocol integration init

---
+++ app/utils/Staking.ts
@@ -0,0 +1,17 @@
+export const protocolData: { [key: string]: ProtocolInfo } = {
+    tortuga: {
+      name: 'Tortuga Finance',
+      stakingInfo: 'Staking on Aptos via Tortuga Finance.',
+      logo: '/apt.png', // Replace with the correct path in the public folder
+    },
+    ankr: {
+      name: 'Ankr',
+      stakingInfo: 'Staking on Polygon via Ankr.',
+      logo: '/ankr.png',
+    },
+    marinade: {
+      name: 'Marinade',
+      stakingInfo: 'Staking on Solana via Marinade.',
+      logo: '/mnde.png',
+    },
+  };
